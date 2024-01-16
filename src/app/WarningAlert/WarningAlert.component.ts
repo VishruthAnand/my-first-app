6fa763ef1cb7eef4265030c0d0a985149ac385b9
320212299 <vishruth.anand@philips.com>
2024-01-16 11:28:56 +0530

    commit file

---
+++ src/app/WarningAlert/WarningAlert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'warning-alert-selector',
+    templateUrl:'./WarningAlert.component.html'
+})
+export class WarningComponent{
+    
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './WarningAlert/WarningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent,
+    ServerComponent,
+    WarningComponent,
+    SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
